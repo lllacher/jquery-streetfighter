2cd09f4c29476af6e7e1375f60cebaa691e2f453
Lisa Lacher <llbender@gmail.com>
2016-09-25 11:31:45 -0500

    fix cool

--- js/app.js
+++ js/app.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
     $('.ryu').mouseenter(function(){
         $('.ryu-still').hide();
         $('.ryu-ready').show();
+        $('.ryu-cool').hide();
     })
     .mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
     })
     .mousedown(function(){
         playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
+        $('.ryu-cool').hide();
         $('.hadouken').finish().show().animate(
             {'left': '1020px'},
             500,
@@ -22,11 +25,13 @@ $(document).ready(function(){
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
+        $('.ryu-cool').hide();
     });
 });
 $(document).keypress(function(e) {
     if(e.which == 88+32) {
         $('.ryu-still').hide();
+        $('.ryu-ready').hide()
         $('.ryu-cool').show();
     }
 });
